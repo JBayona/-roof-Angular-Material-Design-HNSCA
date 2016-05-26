535d833b73dbb9f45949bd8c22172a1727ff3864
JBayona <jordavids_22@hotmail.com>
2016-05-26 17:31:55 -0500

    Continue working on design controller

--- client/app/dialogStyle/dialogStyle.controller.js
+++ client/app/dialogStyle/dialogStyle.controller.js
@@ -1,6 +1,24 @@
 'use strict';
 
 angular.module('roofAngularMaterialDesignApp')
-  .controller('DialogStyleCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('DialogStyleCtrl', ['$scope', '$mdDialog', 'selectedOption', function ($scope, $mdDialog, selectedOption) {
+    
+  	$scope.elements = ['Channel','Paragraph'];
+	  $scope.colors = ['red', 'blue', 'yellow', 'gray', 'black', 'orange', 'pink', 'white'];
+	  $scope.channel = ['Channel','Paragraph'];
+	  $scope.option = ['New Section', 'Current Section'];
+	  $scope.verticalOptions = ['Start', 'Center', 'End'];
+	  $scope.horizontalOptions = ['Start', 'Center', 'End'];
+
+		$scope.hide = function(){
+			$mdDialog.hide();
+		};
+		$scope.cancel = function(){
+			$mdDialog.cancel();
+		};
+		$scope.answer = function(answer){
+    console.log('Option ' + selectedOption + ' was selected');
+			$mdDialog.hide(answer);
+		}
+
+  }]);
