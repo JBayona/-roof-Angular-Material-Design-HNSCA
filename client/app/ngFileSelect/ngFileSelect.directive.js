4a0b5609255b90e341f675a4c2508187360e112e
JBayona <jordavids_22@hotmail.com>
2016-06-09 04:20:47 -0500

    Start working on image

---
+++ client/app/ngFileSelect/ngFileSelect.directive.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('roofAngularMaterialDesignApp')
+  .directive('ngFileSelect', function () {
+    return {
+      link: function (scope, element, attrs) {
+	       element.bind("change", function(e){
+	      
+		        $scope.file = (e.srcElement || e.target).files[0];
+		        $scope.getFile();
+      		})
+      }
+    };
+  });
